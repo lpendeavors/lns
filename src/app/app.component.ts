e7161615e864e81d0dd9ae789f401fd48d8e4d72
Larry Parks <larry.parks@gexaenergy.com>
2019-03-24 12:13:57 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lns-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lns';
+}
